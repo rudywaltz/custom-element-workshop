129018909237e2ebe2766cb53d6694d767cc0fb9
rudywaltz <david.barkoczi@gmail.com>
2019-03-15 10:01:11 +0100

    feat(01): preparates 2nd step

---
+++ 01/step.spec.js
@@ -0,0 +1,17 @@
+var component;
+describe('wc-steps-step', function() {
+  beforeEach(function(done) {
+    component = document.createElement('wc-steps-step');
+    flush(done);
+  });
+
+  afterEach(function() {
+    component.parentNode.removeChild(component);
+  });
+
+  it.skip('should fire update event', function() {
+    var eventSpy = sinon.spy();
+    component.addEventListener('update', eventSpy);
+    document.body.insertBefore(component, document.body.firstChild);
+  });
+});
